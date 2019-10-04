10d474ab5f104d0646f12ea8d95b54ac959718b4
Win Treese <treese@acm.org>
2019-10-03 22:26:36 -0400

    Initial import of aquamacs site as of 10/3/2019

---
+++ testimonials.js
@@ -0,0 +1,63 @@
+var quote = new Array(100);
+
+quote[0] = "<div class=\"quote-quote\">&ldquo;I've been using this for about a week, and have been quite impressed.&rdquo;</div><div class=\"quote-source\"><a href=\"http://moodle.org/mod/forum/discuss.php?d=29583\">Tony Hursh</a>, Urbana, IL, USA</div><div class=\"quote-spec\">Psychologist at UI Urbana-Champaign</div>";
+
+quote[1] = "<div class=\"quote-quote\">&ldquo;Tried it and my head exploded&rdquo;</div><div class=\"quote-source\"><a href=\"\">Ed Heil</a></div><div class=\"quote-spec\"></div>";
+
+quote[2] = "<div class=\"quote-quote\">&ldquo;one of the most advanced LaTeX editing environments available for the Mac&rdquo;</div><div class=\"quote-source\"><a href=\"http://prog.vub.ac.be/~cderoove/geekery/latexemacs.html\">Coen De Roover</a>, Brussels, Belgium</div><div class=\"quote-spec\">Computer Scientist at the Vrije Universiteit Brussel</div>";
+
+quote[3] = "<div class=\"quote-quote\">&ldquo;The differences between 0.9 and 0.9.1 are very impressive & integration with Aqua makes it nicer than the Carbon ports&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.versiontracker.com/users/lp15\">anonymous</a>, VersionTracker</div><div class=\"quote-spec\"></div>";
+
+quote[4] = "<div class=\"quote-quote\">&ldquo;Quite slick&rdquo;</div><div class=\"quote-source\"><a href=\"http://jeffcroft.com/blog/archives/2005/07/which_gui_os_x.php\">Scott Hughes</a>, Dallas, TX, USA</div><div class=\"quote-spec\">Engineer</div>";
+
+quote[5] = "<div class=\"quote-quote\">&ldquo;I'm having the same sort of happy feelings as I did when I was first introduced to emacs and found out how much easier it was than ..., back about 20 years ago. ... Three Cheers for David Reitter and Kevin Walzer.&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.esm.psu.edu/mac-tex/MacOSX-Emacs-Digests/2005/Mac-OS-X-Emacs-Digest-0138-07_06_05.html\">Professor Robert Sloan</a>, Urbana, IL, USA</div><div class=\"quote-spec\">Computer Scientist at U Illinois Urbana-Champaign</div>";
+
+quote[6] = "<div class=\"quote-quote\">&ldquo;It is also much better integrated into OS X. Really nice!&rdquo;</div><div class=\"quote-source\"><a href=\"http://sazius.blogspot.com/2005/05/aquamacs.html\">Mats Sj&ouml;berg</a>, Finland</div><div class=\"quote-spec\"></div>";
+
+quote[7] = "<div class=\"quote-quote\">&ldquo;This is probably the most aqua look and feel version, it doesn't feel like emacs any more&rdquo;</div><div class=\"quote-source\"><a href=\"http://web.engr.oregonstate.edu/~rende/wp/2005/05/19/%e5%b0%86emacs%e8%bf%9b%e8%a1%8c%e5%88%b0%e5%ba%95/\">Deling Ren</a>, Oregon, USA</div><div class=\"quote-spec\">Computer Scientist Oregon State Univerysity</div>";
+
+quote[8] = "<div class=\"quote-quote\">&ldquo; Im Rennen um den besten Editor der Welt zwischen emacs und vi legt emacs ein weiteres Holz in die Brennkammer: Eine Aqua MacOS X Version! Das ist ungef&auml;hr so als w&uuml;rde man einen MAN TGA 26.480 feuerrot spritzen, Magnesium-Felgen mit Slicks aufziehen und ihn bei der Formel 1 antreten lassen: Interessant!&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.ppcnux.com/modules.php?name=News&file=article&sid=5591\">Ralf Saalm&ouml;ller, PPCNUX</a></div><div class=\"quote-spec\"></div>";
+
+quote[9] = "<div class=\"quote-quote\">&ldquo; Aquamacs es la versi&oacute;n de Emacs para OSX que m&aacute;s me ha gustado. Esta perfectamente adaptada a Aqua. Os la recomiendo.&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.faq-mac.com/bitacoras/brocolifresco/?p=92\">Miguel @ Brocoli Fresco</a>, Spain</div><div class=\"quote-spec\"></div>";
+
+quote[10] = "<div class=\"quote-quote\">&ldquo;I absolutely love emacs. It's even better on my new Mac!&rdquo;</div><div class=\"quote-source\"><a href=\"http://sourceforge.net/project/project_donations.php?group_id=138078\">Shervin Fatehi</a>, California, USA</div><div class=\"quote-spec\">Researcher in Chemistry</div>";
+
+quote[11] = "<div class=\"quote-quote\">&ldquo;If you've tried Carbon-Enhanced Emacs and were disappointed, prepare to be happy.&rdquo;</div><div class=\"quote-source\"><a href=\"http://blog.bfccomputing.com/index.php?cat=4\">(blog)</a></div><div class=\"quote-spec\">Resigned to the Bittersweet Truth Blog</div>";
+
+quote[12] = "<div class=\"quote-quote\">&ldquo;I just found Aquamacs last night, and it's spectacular. I didn't go  to bed. Thanks a ton.&rdquo;</div><div class=\"quote-source\"><a href=\"http://article.gmane.org/gmane.emacs.macintosh.osx/1531\">Jonathan Shea</a>, Dartmouth, NH, USA</div><div class=\"quote-spec\">Physicist</div>";
+
+quote[13] = "<div class=\"quote-quote\">&ldquo;Anyhow, I'm typing this entry right now in Aquamacs&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.tbray.org/ongoing/When/200x/2005/11/05/OS-X-Emacs\">Tim Bray</a></div><div class=\"quote-spec\">Co-author of the XML 1.0 recommendation</div>";
+
+quote[14] = "<div class=\"quote-quote\">&ldquo;I come to you today, bringing my overjoyed squeeing (SQUEEEEEEEEE!) of my discovery of Aquamacs Emacs. Allowing me to use my favorite text editor in the context of my favorite operating system, with minimal setup and both mac and emacs key bindings.&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.livejournal.com/users/ataralas/69558.html\">Anonymous (Ataralas)</a>, Chicago</div><div class=\"quote-spec\"></div>";
+
+quote[15] = "<div class=\"quote-quote\">&ldquo;The version that is generally open all the time on my desktop.&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.doug-miller.net/blog/archive/brayonemacs.html\">Doug Miller</a>, Carmel, IN, USA</div><div class=\"quote-spec\">Senior Engineer Analyst, Wishard Health Services</div>";
+
+quote[16] = "<div class=\"quote-quote\">&ldquo;Absolutely fanstastic. Thanks. Even from this old Vim fanatic, this app is highly recommended&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.macupdate.com/reviews.php?id=17630\">chipster (Mac Update)</a></div><div class=\"quote-spec\"></div>";
+
+quote[17] = "<div class=\"quote-quote\">&ldquo;Killer! Thanks for taking the time to enhance emacs - you are life savers.&rdquo;</div><div class=\"quote-source\"><a href=\"http://sourceforge.net/project/project_donations.php?group_id=138078\">rohanr2, Sourceforge User</a></div><div class=\"quote-spec\"></div>";
+
+quote[18] = "<div class=\"quote-quote\">&ldquo;When I started to dive into Lisp a few years ago, I tried to avoid Emacs because I didn't want to learn two big new things at the same time... Nowadays, things seem to be much better: Aquamacs gives you an (original) Emacs but with lots of modifications that turn it into an application that makes it feel much more like a regular Mac application, including again all the standard keystrokes and mouse gestures... I don't think that Emacs is a major stumbling block anymore.&rdquo;</div><div class=\"quote-source\"><a href=\"http://groups.google.com/group/comp.lang.lisp/browse_thread/thread/15e10b4cb954f0d9/bd771082f594ef16\">Pascal Costanza</a>, Brussels, Belgium</div><div class=\"quote-spec\">Belgium Programming Technology Lab, Vrije Universiteit Brussel</div>";
+
+quote[19] = "<div class=\"quote-quote\">&ldquo;At last ! A real merge between Emacs and Mac apps. Bravo !! My mac app of the year.&rdquo;</div><div class=\"quote-source\"><a href=\"http://sourceforge.net/project/project_donations.php?group_id=138078\">Anonymous donor</a></div><div class=\"quote-spec\"></div>";
+
+quote[20] = "<div class=\"quote-quote\">&ldquo;Aquamacs is the absolute best version of emacs for OS X . It really does feel like a native Mac app, but it has all the power and flexibility of traditional emacs.&rdquo;</div><div class=\"quote-source\"><a href=\"http://rajblog.com/blog/?p=391\">Rajib Singh</a>, Atlanta, GA, USA</div><div class=\"quote-spec\"></div>";
+
+quote[21] = "<div class=\"quote-quote\">&ldquo;This is impressive. ... I may have to stop hating emacs...&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.versiontracker.com/php/feedback/article.php?story=20051221155735391\">wgscott</a></div><div class=\"quote-spec\">Versiontracker Comment</div>";
+
+quote[22] = "<div class=\"quote-quote\">&ldquo;Eighth wonder of the world (Emacs is one through seven)&rdquo;</div><div class=\"quote-source\"><a href=\"http://sourceforge.net/project/project_donations.php?group_id=138078\">Anonymous donor</a></div><div class=\"quote-spec\"></div>";
+
+quote[23] = "<div class=\"quote-quote\">&ldquo;Finally have a reason to stop using vanilla Emacs!&rdquo;</div><div class=\"quote-source\"><a href=\"http://www.versiontracker.com/php/feedback/article.php?story=20051221155735391\">Tom Donaldson</a>, System Architect, now full-time geek</div><div class=\"quote-spec\">Donation Comment</div>";
+
+quote[24] = "<div class=\"quote-quote\">&ldquo; Le mode 'carbon' de emacs fesait vraiment office de bout de scotch, il fallait des heures de config pour arriver ‡ quelquechose de convenable... C'Ètait sans savoir qu'il existe un portage NATIF de emacs pour Aqua : Aquamacs, et c'est .... formidable&rdquo;</div><div class=\"quote-source\"><a href=\"\">Theoconcept's blog</a></div><div class=\"quote-spec\"></div>";
+
+quote[25] = "<div class=\"quote-quote\">&ldquo; Die Diss ist fertig, meine Verteidigung war gerade. Und alles Dank Aquamacs Emacs. Das war die richtige Wahl.&rdquo;</div><div class=\"quote-source\"><a href=\"\">Dr. Andreas Heﬂ</a>, Computer Scientist</div><div class=\"quote-spec\">(via E-Mail)</div>";
+
+function writequotes() {var x = document.getElementById("quotes");
+if (x) { x.firstChild.nodeValue = "" }
+var n = Math.round(Math.random()*25);
+document.write("<div id=quotes>");
+document.write(quote[n]);
+if(quote[n].length<500) { var m = n; while(m==n || quote[n].length+quote[m].length>700) 
+{ m =  Math.round(Math.random()*25);}document.write("<br><br>"+quote[m]); }document.write("</div>");
+}
+writequotes();
