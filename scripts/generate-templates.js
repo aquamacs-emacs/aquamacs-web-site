fd81aa60d3ec8f283e74a0bca88f68bd83113cd8
Adam Zerella <adamzerella@users.noreply.github.com>
2020-04-04 16:37:13 +1030

    Add linting to project

--- scripts/generate-templates.js
+++ scripts/generate-templates.js
@@ -5,28 +5,36 @@ const Path = require('path');
 const Nunjucks = require('nunjucks');
 
 // Render the templated .njk files into .html output
-(async() => {
+(async () => {
     const baseDir = Path.join(__dirname, `../public`);
     const outDir = Path.join(__dirname, `../docs`);
     const files = await Fsp.readdir(baseDir);
 
-    Nunjucks.configure(Path.join(__dirname, `../public/`), { autoescape: false });
+    Nunjucks.configure(Path.join(__dirname, `../public/`), {
+        autoescape: false,
+    });
 
     if (!Fs.existsSync(outDir)) {
         Fs.mkdirSync(outDir, { recursive: true });
     }
 
     files
-        .filter(it => it.endsWith(`.njk`))
-        .forEach( async it => {
-            console.log(`Building HTML page for ${ it } ....`);
+        .filter((it) => it.endsWith(`.njk`))
+        .forEach(async (it) => {
+            console.log(`Building HTML page for ${it} ....`);
 
             const templateFilePath = Path.join(__dirname, `../docs/${it}`);
 
             let pageData = await Nunjucks.render(it, {
-                version: require(`../package.json`).version
+                version: require(`../package.json`).version,
             });
 
-            await Fsp.writeFile(`${templateFilePath.substring(0, templateFilePath.length - 4)}.html`, pageData);
+            await Fsp.writeFile(
+                `${templateFilePath.substring(
+                    0,
+                    templateFilePath.length - 4
+                )}.html`,
+                pageData
+            );
         });
 })();
--- scripts/move-assets.js
+++ scripts/move-assets.js
@@ -5,7 +5,7 @@ const Path = require('path');
 const ReadDir = require('fs-readdir-recursive');
 
 // Move asset files into the docs/ directory.
-(async() => {
+(async () => {
     const assetDir = Path.join(__dirname, `../public/assets`);
     const outDir = Path.join(__dirname, `../docs/assets`);
     const outDirImages = Path.join(outDir, `images`);
@@ -18,22 +18,35 @@ const ReadDir = require('fs-readdir-recursive');
         Fs.mkdirSync(outDir, { recursive: true });
     }
 
-    if (!Fs.existsSync(outDirImages) || !Fs.existsSync(outDirJs) || !Fs.existsSync(outDirImagesLatex)) {
+    if (
+        !Fs.existsSync(outDirImages) ||
+        !Fs.existsSync(outDirJs) ||
+        !Fs.existsSync(outDirImagesLatex)
+    ) {
         Fs.mkdirSync(outDirImages, { recursive: true });
         Fs.mkdirSync(outDirJs, { recursive: true });
         Fs.mkdirSync(outDirImagesLatex, { recursive: true });
     }
 
     files
-        .filter( it => !it.endsWith(`.scss`))
-        .forEach( async it => {
+        .filter((it) => !it.endsWith(`.scss`))
+        .forEach(async (it) => {
             let filePath = Path.join(__dirname, `../public/assets`, it);
-            await Fsp.copyFile(filePath, Path.join(__dirname, `../docs/assets`, it));
+            await Fsp.copyFile(
+                filePath,
+                Path.join(__dirname, `../docs/assets`, it)
+            );
         });
 
     // Extra root files
     const favIconFile = Path.join(__dirname, `../public`, `favicon.ico`);
     const manifestFile = Path.join(__dirname, `../public`, `manifest.json`);
-    await Fsp.copyFile(favIconFile, Path.join(__dirname, `../docs`, `favicon.ico`));
-    await Fsp.copyFile(manifestFile, Path.join(__dirname, `../docs`, `manifest.json`));
+    await Fsp.copyFile(
+        favIconFile,
+        Path.join(__dirname, `../docs`, `favicon.ico`)
+    );
+    await Fsp.copyFile(
+        manifestFile,
+        Path.join(__dirname, `../docs`, `manifest.json`)
+    );
 })();
